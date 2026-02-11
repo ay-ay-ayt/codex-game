0443e5efcc00ca5c636b76a2ba5b32d67c7e7f28
ay-ay-ayt <soundlinkmini846@gmail.com>
2026-02-11 17:53:56 +0900

    Replace demo cube with stylized fighter model

--- main.js
+++ main.js
@@ -33,11 +33,83 @@ floor.rotation.x = -Math.PI / 2;
 floor.position.y = 0;
 scene.add(floor);
 
-const cubeGeo = new THREE.BoxGeometry(1, 1, 1);
-const cubeMat = new THREE.MeshStandardMaterial({ color: 0x00aa88, roughness: 0.7, metalness: 0.05 });
-const cube = new THREE.Mesh(cubeGeo, cubeMat);
-cube.position.set(0, 0.5, 0);
-scene.add(cube);
+function createFighter() {
+  const fighter = new THREE.Group();
+  const hullMat = new THREE.MeshStandardMaterial({ color: 0x8f98a3, roughness: 0.55, metalness: 0.35 });
+  const accentMat = new THREE.MeshStandardMaterial({ color: 0x3f4650, roughness: 0.65, metalness: 0.25 });
+  const engineMat = new THREE.MeshStandardMaterial({ color: 0x5f6a78, roughness: 0.45, metalness: 0.45 });
+
+  const fuselage = new THREE.Mesh(new THREE.BoxGeometry(0.24, 0.24, 2.4), hullMat);
+  fuselage.position.set(0, 0.74, 0);
+  fighter.add(fuselage);
+
+  const cockpit = new THREE.Mesh(new THREE.BoxGeometry(0.22, 0.18, 0.58), accentMat);
+  cockpit.position.set(0, 0.88, 0.45);
+  fighter.add(cockpit);
+
+  const nose = new THREE.Mesh(new THREE.ConeGeometry(0.12, 0.52, 4), hullMat);
+  nose.rotation.x = Math.PI / 2;
+  nose.rotation.y = Math.PI / 4;
+  nose.position.set(0, 0.73, 1.46);
+  fighter.add(nose);
+
+  const mainWingGeo = new THREE.BoxGeometry(2.7, 0.08, 1.05);
+  const mainWing = new THREE.Mesh(mainWingGeo, hullMat);
+  mainWing.position.set(0, 0.66, 0.05);
+  fighter.add(mainWing);
+
+  const wingSweepLeft = new THREE.Mesh(new THREE.BoxGeometry(1.05, 0.08, 0.42), accentMat);
+  wingSweepLeft.position.set(-1.13, 0.67, -0.52);
+  wingSweepLeft.rotation.y = 0.55;
+  fighter.add(wingSweepLeft);
+
+  const wingSweepRight = wingSweepLeft.clone();
+  wingSweepRight.position.x = 1.13;
+  wingSweepRight.rotation.y = -0.55;
+  fighter.add(wingSweepRight);
+
+  const tailWingLeft = new THREE.Mesh(new THREE.BoxGeometry(0.95, 0.06, 0.34), hullMat);
+  tailWingLeft.position.set(-0.72, 0.7, -1.05);
+  tailWingLeft.rotation.y = 0.25;
+  fighter.add(tailWingLeft);
+
+  const tailWingRight = tailWingLeft.clone();
+  tailWingRight.position.x = 0.72;
+  tailWingRight.rotation.y = -0.25;
+  fighter.add(tailWingRight);
+
+  const finLeft = new THREE.Mesh(new THREE.BoxGeometry(0.08, 0.48, 0.38), accentMat);
+  finLeft.position.set(-0.28, 0.98, -1.05);
+  finLeft.rotation.z = -0.17;
+  fighter.add(finLeft);
+
+  const finRight = finLeft.clone();
+  finRight.position.x = 0.28;
+  finRight.rotation.z = 0.17;
+  fighter.add(finRight);
+
+  const engineLeft = new THREE.Mesh(new THREE.CylinderGeometry(0.09, 0.11, 0.46, 16), engineMat);
+  engineLeft.rotation.x = Math.PI / 2;
+  engineLeft.position.set(-0.25, 0.71, -1.34);
+  fighter.add(engineLeft);
+
+  const engineRight = engineLeft.clone();
+  engineRight.position.x = 0.25;
+  fighter.add(engineRight);
+
+  const engineGlowLeft = new THREE.Mesh(new THREE.CircleGeometry(0.07, 16), new THREE.MeshBasicMaterial({ color: 0x66ccff }));
+  engineGlowLeft.position.set(-0.25, 0.71, -1.57);
+  fighter.add(engineGlowLeft);
+
+  const engineGlowRight = engineGlowLeft.clone();
+  engineGlowRight.position.x = 0.25;
+  fighter.add(engineGlowRight);
+
+  return fighter;
+}
+
+const fighter = createFighter();
+scene.add(fighter);
 
 // ---- resize ----
 function onResize() {
@@ -53,8 +125,7 @@ function tick(t) {
   const dt = Math.min((t - t0) / 1000, 0.05);
   t0 = t;
 
-  cube.rotation.y += dt * 1.1;
-  cube.rotation.x += dt * 0.6;
+  fighter.rotation.y += dt * 0.45;
 
   renderer.render(scene, camera);
   requestAnimationFrame(tick);
